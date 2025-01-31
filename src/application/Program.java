c799415f8aebdd6c767714e8b7b2a976d898af33
Lucas Honorio <lucashonorioo@hotmail.com>
2025-01-31 19:26:29 -0300

    findyById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,30 +1,21 @@
 package application;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
 		
-		Department obj = new Department(1, "Books");
 		
-		SimpleDateFormat  sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		
-		Date birthDate = sdf.parse("06/05/1995 15:26:00");
-		
-		System.out.println(obj);
-		
-		Seller seller = new Seller(21, "bob", "bob@gmail.com", birthDate, 1000.0, obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
+		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
 		
 	}
